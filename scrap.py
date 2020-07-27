0d6329ddabafaf1377a6a25b767fff56f815e005
desobolevsky <desobolevsky@gmail.com>
2020-07-27 14:53:34 +0300

    improved updating functionality

--- scrap.py
+++ scrap.py
@@ -3,29 +3,29 @@ import time
 
 from bs4 import BeautifulSoup
 
-result = None
-last_check_time = 0
+# request type : last_check_time, result
+rates_data = {'general': {'last_check_time': 0, 'result': None},
+              'banks': {'last_check_time': 0, 'result': None}}
 
 
 # decorator which controls the updating of currencies
-def updater(func):
-    def wrapper():
-        global result
-        global last_check_time
-        # update currencies only if 30 minutes passed
-        if time.time() - last_check_time > 30 * 60:
-            print('getting new info')
-            last_check_time = time.time()
-            result = func()
-            return result
-        else:
-            return result
+def decorator(request_type):
+    def updater(func):
+        def wrapper():
+            # update rates only if 30 minutes passed
+            if time.time() - rates_data[request_type]['last_check_time'] > 30 * 60:
+                rates_data[request_type]['last_check_time'] = time.time()
+                rates_data[request_type]['result'] = func()
 
-    return wrapper
+            return rates_data[request_type]['result']
+
+        return wrapper
+
+    return updater
 
 
 # scraping from finance.tut.by
-@updater
+@decorator('general')
 def scrap_general_currencies():
     page = requests.get("https://finance.tut.by/kurs/minsk/")
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -37,7 +37,6 @@ def scrap_general_currencies():
     # every 4 elements in rates_tutby_scrapped contain info about 1 currency
     # the 1st and 2nd elements are buy and sell rate, whereas 3rd and 4th are NBRB (НБРБ) rates,
     # that's why keep only 1st and 2nd for every 4 elements
-
     rates_tutby_clean = [rates_tutby_scrapped[i] for i in range(len(rates_tutby_scrapped)) if i % 4 == 0 or i % 4 == 1]
 
     currency_names = ['Dollar', 'Euro', '100 RUB', '100 UAH', '10 PLN', 'Funt', 'Frank', '100 en', '100 CZK', '10 SWD',
@@ -51,7 +50,7 @@ def scrap_general_currencies():
 
 
 # scraping from myfin.by
-@updater
+@decorator('banks')
 def scrap_bank_currencies():
     page = requests.get("https://myfin.by/currency/minsk", headers={'User-agent': 'your bot 0.1'})
     soup = BeautifulSoup(page.content, 'html.parser')
